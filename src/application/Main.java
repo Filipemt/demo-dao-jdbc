7f2046b8f8d05f21515bbc0a4da6df5c8f669fea
Filipemtb <filipera3@gmail.com>
2024-06-17 12:52:54 -0300

    create: add department class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,15 @@
 package application;
 
+import model.entities.Department;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Department department = new Department(1, "Books");
+
+        System.out.println(department);
     }
 }
+
+
+
